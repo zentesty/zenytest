fb4306e753e760a92ec55039e49b66cc5a8451d9
Zeny Test <zenteliatest@gmail.com>
2018-09-21 09:03:41 -0400

    Change Master 22

--- src/main/java/Zoumba.java
+++ src/main/java/Zoumba.java
@@ -10,7 +10,7 @@ public class Zoumba {
     static private int objCount = 0;
     private String name = "";
     private int size = 0;
-    private int nType = 2;
+    private int nType = 22;
 
 
     public Zoumba(String name, int size, int type) {
